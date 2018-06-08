8e1b782820bc14abc136a32ab408cd1a4824c615
Parinya <pond.yodming@gmail.com>
2018-06-08 15:53:32 +0700

    อัฟเดตหน้า BRANDS

--- brands.php
+++ brands.php
@@ -13,17 +13,37 @@
     <body>
     <div class="wrapper">
     <!-- CONTENT -->
-      <div class="container-fluid px-5">
-        <div class="row mt-4 brand-border-bottom">
-          <div class="brand-font1">ช็อปตามแบรนด์</div>
+        <div class="container-fluid">
+            <div class="row mt-4 brand-border-bottom brand-ml-mr">
+              <div class="brand-font">ช็อปตามแบรนด์</div>
+            </div>
+            <div class="row text-center brand-mt">
+                <div class="col-xl-3 col-lg-4 col-md-6 mt-4">
+                    <div>
+                      <img class = "brand-border-image" src="https://www.iclickstore.com/wp-content/uploads/2017/04/energy-146x60.png" width="250" height="200" alt="ENERGY">
+                    </div>
+                    <button class="mt-4 brand-button">เลือก</button>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-md-6 mt-4">
+                    <div>
+                      <img class = "brand-border-image" src="https://www.iclickstore.com/wp-content/uploads/2017/04/logo-146x60.png" width="250" height="200" alt="ENERGY Premium">
+                    </div>
+                    <button class="mt-4 brand-button">เลือก</button>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-md-6 mt-4">
+                    <div>
+                      <img class = "brand-border-image" src="https://www.iclickstore.com/wp-content/uploads/2017/04/logo-146x60.png" width="250" height="200" alt="ENERGY Premium">
+                    </div>
+                    <button class="mt-4 brand-button">เลือก</button>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-md-6 mt-4">
+                    <div>
+                      <img class = "brand-border-image" src="https://www.iclickstore.com/wp-content/uploads/2017/04/logo-146x60.png" width="250" height="200" alt="ENERGY Premium">
+                    </div>
+                    <button class="mt-4 brand-button">เลือก</button>
+                </div>
+            </div>
         </div>
-        <div class="row">
-          <div class="col-lg-3">drgdzth</div>
-          <div class="col-lg-3"></div>
-          <div class="col-lg-3"></div>
-          <div class="col-lg-3"></div>
-        </div>
-      </div>
     </div>
     <!-- END CONTENT -->
 
