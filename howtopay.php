38f3ceb8dd40d79fcdd01f21fb29528707508bc9
Parinya <pond.yodming@gmail.com>
2018-06-11 09:20:32 +0700

    update howtopay.php (pond)

--- howtopay.php
+++ howtopay.php
@@ -13,19 +13,74 @@
     <div class="wrapper">
     <!-- CONTENT -->
         <div class="row">
-           <div class="container-fluid mb-5">
-                <div class="row mt-4 htp-border-bottom htp-ml-mr">
-                  <div class="htp-font">วิธีการชำระเงิน</div>
+           <div class="p-5 mb-5">
+                <div class="row mt-4 htp-border-bottom">
+                  <div class="htp-font1">วิธีการชำระเงิน</div>
                 </div>
             <div class="row  htp-mt justify-content-center">
-                <div class="col-xl-4 col-lg-4 col-md-6 mt-4 htp-border-image">
-                    <img  width="700" height="200" src="https://www.iclickstore.com/wp-content/uploads/2017/03/1-1.png" class="vc_single_image-img attachment-full" alt="" srcset="https://www.iclickstore.com/wp-content/uploads/2017/03/1-1.png 700w, https://www.iclickstore.com/wp-content/uploads/2017/03/1-1-300x86.png 300w, https://www.iclickstore.com/wp-content/uploads/2017/03/1-1-600x171.png 600w" sizes="(max-width: 700px) 100vw, 700px">
-                    <div class="">wrgert</div>
+                <div class="col-xl-5 col-lg-5 col-md-5 mt-4 htp-border-image p-3">
+                     <img width="100%" height="auto" src="https://www.iclickstore.com/wp-content/uploads/2017/03/1-1.png" class="vc_single_image-img attachment-full" alt="" srcset="https://www.iclickstore.com/wp-content/uploads/2017/03/1-1.png 700w, https://www.iclickstoreiv.com/wp-content/uploads/2017/03/1-1-300x86.png 300w, https://www.iclickstore.com/wp-content/uploads/2017/03/1-1-600x171.png 600w" sizes="(max-width: 700px) 100vw, 700px">
+
+                             <div class="row">
+                                <div class="col-xl-2"></div>
+                                <div class="col-xl-3 col-lg-4">
+                                   <div class="htp-font2">ชื่อบัญชี :</div>
+                                </div>
+                                <div class="col-xl-7 col-lg-8">
+                                   <div class="htp-font3">บจ.โกล์ด เบลล์ ซิสเท็ม</div>
+                                </div>
+                             </div>
+
+                            <div class="row">
+                                <div class="col-xl-2"></div>
+                                <div class="col-xl-3 col-lg-5">
+                                   <div class="htp-font2">ธนาคาร :</div>
+                                </div>
+                                <div class="col-xl-7 col-lg-7">
+                                   <div class="htp-font3">กรุงเทพ</div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-xl-2"></div>
+                                <div class="col-xl-3 col-lg-4">
+                                   <div class="htp-font2">สาขา :</div>
+                                </div>
+                                <div class="col-xl-7 col-lg-8">
+                                   <div class="htp-font3">พาราไดซ์ พาร์ค</div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-xl-2"></div>
+                                <div class="col-xl-3 col-lg-4">
+                                   <div class="htp-font2">ประเภทบัญชี :</div>
+                                </div>
+                                <div class="col-xl-7 col-lg-8">
+                                   <div class="htp-font3">เงินฝากสะสมทรัพย์</div>
+                                </div>
+                            </div>
+
+                             <div class="row">
+                                <div class="col-xl-2"></div>
+                                <div class="col-xl-3 col-lg-5">
+                                  <div class="htp-font2">เลขบัญชี :</div>
+                                </div>
+                                <div class="col-xl-7 col-lg-7">
+                                   <div class="htp-font3">068-7-20555-9</div>
+                                </div>
+                            </div>       
                 </div>
-                <div class="col-xl-1"></div>
-                <div class="col-xl-4 col-lg-4 col-md-6 mt-4 htp-border-image">
-                   <img  width="700" height="200" src="https://www.iclickstore.com/wp-content/uploads/2017/03/1-1.png" class="vc_single_image-img attachment-full" alt="" srcset="https://www.iclickstore.com/wp-content/uploads/2017/03/1-1.png 700w, https://www.iclickstore.com/wp-content/uploads/2017/03/1-1-300x86.png 300w, https://www.iclickstore.com/wp-content/uploads/2017/03/1-1-600x171.png 600w" sizes="(max-width: 700px) 100vw, 700px">
-                   <div class="">wrgert</div>
+                <div class="col-xl-1 col-lg-1 col-md-1"></div>
+                <div class="col-xl-5 col-lg-5 col-md-5 mt-4 htp-border-image p-3">             
+                     <img width="100%" height="auto" src="https://www.iclickstore.com/wp-content/uploads/2017/03/PayPal-1.png" class="vc_single_image-img attachment-full" alt="" srcset="https://www.iclickstore.com/wp-content/uploads/2017/03/PayPal-1.png 300w, https://www.iclickstore.com/wp-content/uploads/2017/03/PayPal-1-114x30.png 114w, https://www.iclickstore.com/wp-content/uploads/2017/03/PayPal-1-228x60.png 228w">
+                     <div class="mt-3 text-center">
+                        <div class="htp-font2">ท่านสามารถชำระเงินผ่าน Paypal ได้</div>   
+                        <div class="htp-font3">โดยเลือกในขั้นตอนการชำระเงิน</div>     
+                     </div>
+                     <div class="row">
+                        
+                     </div>
                 </div>
             </div>
         </div>
