0dd6ae7f5100c90398b9d6e6cca69c39238e8a38
haritleksopee <33746030+haritleksopee@users.noreply.github.com>
2018-06-13 17:18:37 +0700

    add product-detail.php and product-list.php 17.18 13/06/2561

---
+++ product-detail.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- START HEADER -->
+    <?php include_once('inc/header.php'); ?>
+    <!-- END HEADER -->
+  </head>
+
+  <body>
+  <!-- START NAVBAR -->
+    <?php include_once('inc/navbar.php'); ?>
+    <!-- END NAVBAR -->
+    <div class="wrapper">
+     <!-- CONTENT  -->
+
+
+      <div class="home-size-web productlist-my-5">
+                
+        <div class="col-12">
+          <div class="prodetail-link">
+            <a href="#" class="prodetail-link-color">หน้าหลัก</a>
+            <font class="prodetail-link-color"> > </font>
+            <a href="#" class="prodetail-link-color">รายการสินค้า</a>
+            <font class="prodetail-link-color"> > </font>
+            <a href="#" class="prodetail-link-color">หมวดสินค้าหลัก</a>
+            <font class="prodetail-link-color"> > </font>
+            <a href="#" class="prodetail-link-color prodetail-link-active">พาวเวอร์แบงค์</a>
+          </div>
+          <div><hr style="border: 2px solid #d6d6d6;"></div>
+        </div>
+
+
+
+        <div class="row">
+          <div class="col-md-5">
+            <div class="prodetail-img-bands">
+              <figure class="zoom" onmousemove="zoom(event)" style="background-image: url(assets/images/slide/Slider-02.png)">
+                <img src="assets/images/slide/Slider-02.png" />
+              </figure>
+            </div>
+          </div>
+
+
+
+          <div class="col-md-7">
+              
+              <div class="row">
+                <div class="col-9">
+                  <div class="text-dark-purple">
+                    <h2 class="prodetail-text-head ">พาวเวอร์แบงค์ ชาร์จเร็ว M2 12000mAh หลากสีสัน</h2>
+                  </div>
+                </div>
+                <div class="col-3">
+                  <div class="text-purple text-right">
+                    <h2 style="font-weight: 600;">810 บาท</h2>
+                  </div>
+                </div>
+              </div>
+
+
+              <div class="prodetail-mx-55 text-purple">
+                <font>สถานะ : <b>เปิดขาย</b></font>
+              </div>
+
+
+              <div class="prodetail-mx-55 prodetail-mt-20 text-secondary">
+                <p class="prodetail-mb-0">– พาวเวอร์แบงค์ ความจุ 12,000mAh</p>
+                <p class="prodetail-mb-0">– ชาร์จเร็ว ปล่อยกระแสไฟได้สูงสุดถึง 2.1A</p>
+                <p class="prodetail-mb-0">– มีพอร์ต USB จำนวน 2 พอร์ต</p>
+                <p class="prodetail-mb-0">– สามารถชาร์จอุปกรณ์ได้ 2 พอร์ตพร้อมกันได้</p>
+                <p class="prodetail-mb-0">– ไฟแสดงความจุแบตเตอรี่</p>
+                <p class="prodetail-mb-0">– สามารถชาร์จ Tablet หรือ Smart Phone ได้</p>
+                <p class="prodetail-mb-0">– มีสายชาร์จในกล่อง</p>
+                <p class="prodetail-mb-0">– Port สำหรับชาร์จไฟเข้า แบบ Micro USB (Android)</p>
+                <p class="prodetail-mb-0">– รับประกันสินค้านาน 6 เดือน</p>
+              </div>
+
+
+
+              <div class="prodetail-mx-55 prodetail-mt-20 text-secondary">
+                <p class="prodetail-mb-0">แบรนด์ : <img src="assets/images/Energy_logo.png" class="prodetail-logo-barnd"></p>
+              </div>
+
+              <div class="prodetail-mx-55 prodetail-mt-20 text-secondary">
+                <p class="prodetail-mb-0">รหัสสินค้า : <font class="text-dark-purple">EP1PB17024</font></p>
+              </div>
+
+
+              <div class="prodetail-mx-55 prodetail-mt-20 text-secondary">
+                <p class="prodetail-mb-0">
+                    <div class="form-inline"> สี :
+                        <div class="home-change-color mr-1" style="margin-left: 25px;">
+                          <a href="#" class="change-color" style="background-color: #fff;"></a>
+                        </div>
+                        <div class="home-change-color mr-1">
+                          <a href="#" class="change-color" style="background-color: #000;"></a>
+                        </div>
+                        <div class="home-change-color mr-1">
+                          <a href="#" class="change-color" style="background-color: #1d5ed4;"></a>
+                        </div>
+                    </div>
+                </p>
+              </div>
+
+          </div>
+        </div>
+
+
+
+      </div>
+
+     <!-- END CONTENT  -->
+    </div> <!-- wrapper -->
+
+    <!-- START FOOTER -->
+    <?php include_once('inc/footer.php'); ?>
+    <!-- END FOOTER -->
+
+    <!-- START FOOTER SCRIPT -->
+    <?php include_once('inc/footer-script.php'); ?>
+    <!-- END FOOOTER SCRIPT -->
+
+
+  </body>
+</html>
+
+
+
+
