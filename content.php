4202ea58be929ee964e410dbfeace86005ab02e8
gggia <gggia36@gmail.com>
2018-06-14 17:06:38 +0700

    up Payconfirm.php about.php article.php assets/css/custom.css content.php login.php
    by gia
    14/06/61
    17.06

--- content.php
+++ content.php
@@ -12,130 +12,143 @@
     <?php include_once('inc/navbar.php'); ?>
     <!-- END NAVBAR -->
     <div class="wrapper">
-    <!-- CONTENT -->
+    <!-- conTENT -->
+
+
+<div class="con-size-web">
+
     
     <div class="container-fluid">
       <div class="row">
-          <div class="col-lg-2 mt-4">
-            <h2 class="text-center con_text_haed">บทความ</h2>
-          </div>
-        <div class="col-lg-8 mt-4"></div>
-          <div class="col-lg-2 mt-4">
-            <h4 class="text-center con_text_haed_1">2018-05-20</h4>
-          </div>
+        <div class="col-lg-6 ">
+          <h1 class="text-center text-xl-left con_text_haed">บทความ</h1>
+        </div>
+        <div class="col-lg-6 ">
+          <h1 class="text-center text-xl-right  con_text_haed_1">2018-05-20</h1>
+        </div>
       </div>
    
-        <hr class="CON_hr_width">
-      <div class="row CON_t">
-        <div class="col-lg-4">
-          <div class="CON_fram_img ">
-            <img class="CON_img_1" src="http://via.placeholder.com/850x900">
+        <hr class="con_hr_width">
+
+  <div class="row">
+    <div class="col-lg-12">
+      <center>
+        <div class="row">
+          <div class="col-lg-4  mt-xl-4">
+            <div class="con_fram_img ">
+              <img class="con_img_1" src="http://via.placeholder.com/850x900">
+            </div>   
           </div>
-          
-          
-        </div>
-        <div class="col-lg-8 CON_let mt-md-4">
-          <div class="CON-fix">
-            <h2 class="card-title CON_text_h">What is Lorem Ipsum?</h2>
-            <p class="CON_hay ">นักธุรกิจที่ประสบความสำเร็จคนหนึ่งเริ่มแก่ตัวลง และต้องการหาคนมาสืบทอดธุรกิจ 
-            แทนที่เขาจะเลือกผู้อำนวยการ หรือ ลูกของเขา แต่เขาตัดสินใจที่จะทำบางอย่างที่แตกต่างออกไป
-            เขาเรียกนักบริหารหนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุรกิจที่ประสบความสำเร็จคนหนึ่งเริ่มแก่ตัวลง และต้องการหาคนมาสืบทอดธุรกิจ 
-            แทนที่เขาจะเลือกผู้อำนวยการ หรือ ลูกของเขา แต่เขาตัดสินใจที่จะทำบางอย่างที่แตกต่างออกไป
-            เขาเรียกนักบริหารหนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุรกิจที่ประสบความสำเร็จคนหนึ่งเริ่มแก่ตัวลง และต้องการหาคนมาสืบทอดธุรกิจ 
-            แทนที่เขาจะเลือกผู้อำนวยการ หรือ ลูกของเขา แต่เขาตัดสินใจที่จะทำบางอย่างที่แตกต่างออกไป
-            เขาเรียกนักบริหารหนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุรกิจที่ประสบความสำเร็จคนหนึ่งเริ่มแก่ตัวลง และต้องการหาคนมาสืบทอดธุรกิจ 
-            แทนที่เขาจะเลือกผู้อำนวยการ หรือ ลูกของเขา แต่เขาตัดสินใจที่จะทำบางอย่างที่แตกต่างออกไป
-            เขาเรียกนักบริหารหนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุรกิจที่ประสบความสำเร็จคนหนึ่งเริ่มแก่ตัวลง และต้องการหาคนมาสืบทอดธุรกิจ 
-            แทนที่เขาจะเลือกผู้อำนวยการ หรือ ลูกของเขา แต่เขาตัดสินใจที่จะทำบางอย่างที่แตกต่างออกไป
-            เขาเรียกนักบริหารหนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุรกิจที่ประสบความสำเร็จคนหนึ่งเริ่มแก่ตัวลง และต้องการหาคนมาสืบทอดธุรกิจ 
-            แทนที่เขาจะเลือกผู้อำนวยการ หรือ ลูกของเขา แต่เขาตัดสินใจที่จะทำบางอย่างที่แตกต่างออกไป
-            เขาเรียกนักบริหารหนุ่มๆในบริษัทของเขามารวมกัน และพูดว่า</p>
+
+          <div class="col-lg-8  mt-xl-4  text-xl-left">
+            <div class="con-fix ">
+              <h2 class="card-title con_text_h mt-5 mt-xl-0 ">What is Lorem Ipsum?</h2>
+              <p class="con_hay ">นักธุรกิจที่ประสบความสำเร็จคนหนึ่งเริ่มแก่ตัวลง และต้องการหาคนมาสืบทอดธุรกิจ 
+              แทนที่เขาจะเลือกผู้อำนวยการ หรือ ลูกของเขา แต่เขาตัดสินใจที่จะทำบางอย่างที่แตกต่างออกไป
+              เขาเรียกนักบริหารหนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุรกิจที่ประสบความสำเร็จคนหนึ่งเริ่มแก่ตัวลง และต้องการหาคนมาสืบทอดธุรกิจ 
+              แทนที่เขาจะเลือกผู้อำนวยการ หรือ ลูกของเขา แต่เขาตัดสินใจที่จะทำบางอย่างที่แตกต่างออกไป
+              เขาเรียกนักบริหารหนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุรกิจที่ประสบความสำเร็จคนหนึ่งเริ่มแก่ตัวลง และต้องการหาคนมาสืบทอดธุรกิจ 
+              แทนที่เขาจะเลือกผู้อำนวยการ หรือ ลูกของเขา แต่เขาตัดสินใจที่จะทำบางอย่างที่แตกต่างออกไป
+              เขาเรียกนักบริหารหนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุรกิจที่ประสบความสำเร็จคนหนึ่งเริ่มแก่ตัวลง และต้องการหาคนมาสืบทอดธุรกิจ 
+              แทนที่เขาจะเลือกผู้อำนวยการ หรือ ลูกของเขา แต่เขาตัดสินใจที่จะทำบางอย่างที่แตกต่างออกไป
+              เขาเรียกนักบริหารหนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุรกิจที่ประสบความสำเร็จคนหนึ่งเริ่มแก่ตัวลง และต้องการหาคนมาสืบทอดธุรกิจ 
+              แทนที่เขาจะเลือกผู้อำนวยการ หรือ ลูกของเขา แต่เขาตัดสินใจที่จะทำบางอย่างที่แตกต่างออกไป
+              เขาเรียกนักบริหารหนุ่มๆในบริษัทของเขามารวมกัน และพูดว่านักธุรกิจที่ประสบความสำเร็จคนหนึ่งเริ่มแก่ตัวลง และต้องการหาคนมาสืบทอดธุรกิจ 
+              แทนที่เขาจะเลือกผู้อำนวยการ หรือ ลูกของเขา แต่เขาตัดสินใจที่จะทำบางอย่างที่แตกต่างออกไป
+              เขาเรียกนักบริหารหนุ่มๆในบริษัทของเขามารวมกัน และพูดว่า</p>
+            </div> 
           </div> 
-        </div> 
+        </div>
       </div>
-    </div>
+    </center>
+  </div>
+</div>
 
       <div class="container">
         <div class="row">
           <div class="col-lg-12 col-12 ">
              <div class="text-center">
-                <button type="button" class="btn btn-blue CON-lo">ย้อนกลับ</button>
+                <button type="button" class="btn btn-blue con-lo">ย้อนกลับ</button>
              </div>
           </div>
         </div>
       </div> 
 
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-3">
-            <h3 class=" text-center CON_top_mer-1">เรื่องที่คุณอาจสนใจ</h3>
-          </div>
-          <div class="col-lg-9"></div>
-        </div>
-         <div class="row ">
-           <div class="col-lg-4 mt-lg-5">
-            <div class="CON_c_a CON_top_mer" >
-              <div class="CON_fram_img-l">
-                <a href="#"><img class="card-img-top CON_img-card-l" src="http://via.placeholder.com/350x150" alt="Card image cap"></a>
+  <div class="container-fluid">
+    <div class="row ">
+      <div class="col-lg-12">
+        <h3 class=" text-center text-xl-left con_top_mer-1  mb-3 mb-xl-0">เรื่องที่คุณอาจสนใจ</h3>
+      </div>
+    </div>
+
+       <div class="row text-center text-xl-left">
+          <div class="col-lg-4 mt-lg-5">
+            <div class="con_c_a con_top_mer" >
+              <div class="con_fram_img-l">
+                <a href="#"><img class="card-img-top con_img-card-l" src="http://via.placeholder.com/350x150" alt="Card image cap"></a>
               </div>
-              <div class="card-body">
-                <a href="#"><h5 class="card-title CON_text_h">What is Lorem Ipsum?</h5>
-                <p class="card-text CON_color">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's</p></a>
-                <a href="#" class="CON_color-1">2015-05-20</a>
+              <div class="card-body text-xl-left">
+                <a href="#"><h5 class="card-title con_text_h">What is Lorem Ipsum?</h5>
+                <p class="card-text con_color">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's</p></a>
+                <a href="#" class="con_color-1">2015-05-20</a>
               </div>
             </div>
           </div>  
 
            <div class="col-lg-4 mt-lg-5">
-            <div class="CON_c_a CON_top_mer" >
-              <div class="CON_fram_img-l">
-                <a href="#"><img class="card-img-top CON_img-card-l" src="http://via.placeholder.com/350x150" alt="Card image cap"></a>
+            <div class="con_c_a con_top_mer" >
+              <div class="con_fram_img-l">
+                <a href="#"><img class="card-img-top con_img-card-l" src="http://via.placeholder.com/350x150" alt="Card image cap"></a>
               </div>
-              <div class="card-body">
-                <a href="#"><h5 class="card-title CON_text_h">What is Lorem Ipsum?</h5>
-                <p class="card-text CON_color">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's</p></a>
-                <a href="#" class="CON_color-1">2015-05-20</a>
+              <div class="card-body text-xl-left">
+                <a href="#"><h5 class="card-title con_text_h">What is Lorem Ipsum?</h5>
+                <p class="card-text con_color">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's</p></a>
+                <a href="#" class="con_color-1">2015-05-20</a>
               </div>
             </div>
           </div>  
 
          <div class="col-lg-4 mt-lg-5">
-            <div class="CON_c_a CON_top_mer" >
-              <div class="CON_fram_img-l">
-                <a href="#"><img class="card-img-top CON_img-card-l" src="http://via.placeholder.com/350x150" alt="Card image cap"></a>
+            <div class="con_c_a con_top_mer" >
+              <div class="con_fram_img-l">
+                <a href="#"><img class="card-img-top con_img-card-l" src="http://via.placeholder.com/350x150" alt="Card image cap"></a>
               </div>
               <div class="card-body">
-                <a href="#"><h5 class="card-title CON_text_h">What is Lorem Ipsum?</h5>
-                <p class="card-text CON_color">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's</p></a>
-                <a href="#" class="CON_color-1">2015-05-20</a>
+                <a href="#"><h5 class="card-title con_text_h">What is Lorem Ipsum?</h5>
+                <p class="card-text con_color">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's</p></a>
+                <a href="#" class="con_color-1">2015-05-20</a>
               </div>
             </div>
           </div>  
         </div> 
       </div>
 
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6 mb-5">
-             <div class="CON_fram_11">
-              <img class="art_img_1" src="https://www.iphones.ru/wp-content/uploads/2016/06/LightningCableInsideM.jpg">
-             </div>
-           </div>
-           <div class="col-lg-6 mb-5">
-             <div class="CON_fram_11">
-              <img class="CON_img_11" src="https://www.iphones.ru/wp-content/uploads/2015/10/Lightning_repair_main.jpg">
-             </div>
-           </div> 
-        </div>    
-      </div>
-
-      <div>
-        <img src="https://sites.google.com/site/work01nayathakhwamsaxad/_/rsrc/1487859139460/config/customLogo.gif?revision=8"  width="100%" height="400px">
-      </div>
+            <div class="row con-my-5 ">
+              <div class="col-md-6 col-12 mb-3">
+                <div class="con_fram_11">
+                  <img src="assets/images/product/HTB1.jpg" class="con_img_11">
+                </div>
+              </div>
+              <div class="col-md-6 col-12 mb-3">
+                <div class="con_fram_11">
+                  <img src="assets/images/product/HTB1.jpg" class="con_img_11">
+                </div>
+              </div>
+            </div>
 
 
 
-    <!-- END CONTENT -->
+</div> 
+          <div class="con_img_last-9">
+            <img  src="assets/images/slide/Slider-02.png" class="con-img-last">
+          </div>
+    
+
+      
+
+
+
+    <!-- END conTENT -->
     </div>
 
     <!-- START FOOTER -->
@@ -161,7 +174,7 @@ window.onload = function(){
 
 
  // รูป
-       $( '.CON_img-card-l' ).each( function () { 
+       $( '.con_img-card-l' ).each( function () { 
            var iw = $(this).width(); 
            var ih = $(this).height(); 
            if(iw > ih){ 
