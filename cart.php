83c2ee70f67d4cbe059c09f7b6676da73548838a
haritleksopee <33746030+haritleksopee@users.noreply.github.com>
2018-06-15 16:20:44 +0700

    add new link 16.20 15/06/2561

---
+++ cart.php
@@ -0,0 +1,269 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- START HEADER -->
+    <?php include_once('inc/header.php'); ?>
+    <!-- END HEADER -->
+  </head>
+
+  <body>
+  <!-- START NAVBAR -->
+    <?php include_once('inc/navbar.php'); ?>
+    <!-- END NAVBAR -->
+    <div class="wrapper">
+      <div class="home-size-web">
+
+        <!-- HEAD -->
+        <div class="col-12">
+          <div class="text-purple text-md-left text-center">
+            <h1>ตะกร้าสินค้า</h1>
+          </div>
+          <div><hr style="border: 2px solid #d6d6d6;"></div>
+        </div>
+        <!-- END HEAD -->
+
+        <!-- CONTENT -->
+          <div class="row cart-my-60">
+
+            <div class="col-12" style="position: relative;">
+              <div class="cart-circle">
+                <a href="#" class="cart-circle-color">
+                  <i class="fa fa-times-circle-o" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+
+            <div class="col-xl-3 col-lg-2 col-12">
+              <div class="flex-img-cart">
+                <img src="assets/images/product/1.jpg" class="cart-img">
+              </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-7 col-12 cart-set-line ">
+              <div class="order-text-set-h text-md-left text-center order-mt-1024">
+                <label class="text-dark-purple">พาวเวอร์แบงค์ ชาร์จเร็ว M2 12000mAh หลากสี</label>
+              </div>
+              <div class="order-text-set-b text-md-left text-center">
+                <label class="text-secondary">สถานะ : </label>
+                <label class="text-purple">เปิดขาย</label>
+              </div>
+              <div class="form-inline product-form-center-sm cart-inline-text414">    
+                <label class="text-secondary order-text-set-b cart-ml-50">สี : </label>
+                <div class="home-change-color mr-1" style="margin-left: 25px;">
+                  <a href="#" class="change-color" style="background-color: #fff;"></a>
+                </div>
+                <div class="home-change-color mr-1">
+                  <a href="#" class="change-color" style="background-color: #000;"></a>
+                </div>
+                <div class="home-change-color mr-1 home-change-color-active">
+                  <a href="#" class="change-color  change-color-active" style="background-color: #1d5ed4;"></a>
+                </div>
+              </div>
+              <div class="prodetail-inline-text cart-inline-text414">
+                <div class="cart-icon-color">
+                  <i class="flaticon-truck"></i>
+                </div>
+                <div class="cart-shipping text-secondary">
+                  <p>Free Shipping</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-5 col-12 cart-set-line">
+              <div class="order-text-set-h text-md-right text-center">
+                <label class="text-purple">810 บาท</label>
+              </div>
+
+
+              <div class="row mt-3">
+                <div class="col-xl-3 col-lg-3 col-12 px-0 pt-2">
+                  <div class="text-lg-left text-center">
+                   <label class="text-secondary order-text-set-b">จำนวน : </label>
+                  </div>
+                </div>
+                <div class="col-xl-7 col-lg-8 col-12 px-0 pt-2">
+                  <div class="text-center" style="margin-top: -10px;">
+                    <button type="button" class="btn prodetail-minus"><i class="fa fa-minus" aria-hidden="true"></i></button>
+                    <input type="text" class="profile-form prodetail-number text-center" name="name" value="1">
+                    <button type="button" class="btn prodetail-plus"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                  </div>
+                </div>
+                <div class="col-xl-2 col-lg-1 col-12 px-0 pt-2">
+                  <div class="text-lg-right text-center">
+                    <label class="text-secondary order-text-set-b">ชิ้น</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="px-3"><hr style="border: 1px solid #d6d6d6;"></div>
+
+
+          <div class="row cart-my-60">
+
+            <div class="col-12" style="position: relative;">
+              <div class="cart-circle">
+                <a href="#" class="cart-circle-color">
+                  <i class="fa fa-times-circle-o" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+
+            <div class="col-xl-3 col-lg-2 col-12">
+              <div class="flex-img-cart">
+                <img src="assets/images/product/1.jpg" class="cart-img">
+              </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-7 col-12 cart-set-line ">
+              <div class="order-text-set-h text-md-left text-center order-mt-1024">
+                <label class="text-dark-purple">พาวเวอร์แบงค์ ชาร์จเร็ว M2 12000mAh หลากสี</label>
+              </div>
+              <div class="order-text-set-b text-md-left text-center">
+                <label class="text-secondary">สถานะ : </label>
+                <label class="text-purple">เปิดขาย</label>
+              </div>
+              <div class="form-inline product-form-center-sm cart-inline-text414">    
+                <label class="text-secondary order-text-set-b cart-ml-50">สี : </label>
+                <div class="home-change-color mr-1" style="margin-left: 25px;">
+                  <a href="#" class="change-color" style="background-color: #fff;"></a>
+                </div>
+                <div class="home-change-color mr-1">
+                  <a href="#" class="change-color" style="background-color: #000;"></a>
+                </div>
+                <div class="home-change-color mr-1 home-change-color-active">
+                  <a href="#" class="change-color  change-color-active" style="background-color: #1d5ed4;"></a>
+                </div>
+              </div>
+              <div class="prodetail-inline-text cart-inline-text414">
+                <div class="cart-icon-color">
+                  <i class="flaticon-truck"></i>
+                </div>
+                <div class="cart-shipping text-secondary">
+                  <p>Free Shipping</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-5 col-12 cart-set-line">
+              <div class="order-text-set-h text-md-right text-center">
+                <label class="text-purple">810 บาท</label>
+              </div>
+
+
+              <div class="row mt-3">
+                <div class="col-xl-3  col-lg-3 col-12 col-12 px-0 pt-2">
+                  <div class="text-lg-left text-center">
+                   <label class="text-secondary order-text-set-b">จำนวน : </label>
+                  </div>
+                </div>
+                <div class="col-xl-7  col-lg-8 col-12 col-12 px-0 pt-2">
+                  <div class="text-center" style="margin-top: -10px;">
+                    <button type="button" class="btn prodetail-minus"><i class="fa fa-minus" aria-hidden="true"></i></button>
+                    <input type="text" class="profile-form prodetail-number text-center" name="name" value="1">
+                    <button type="button" class="btn prodetail-plus"><i class="fa fa-plus" aria-hidden="true"></i></button>
+                  </div>
+                </div>
+                <div class="col-xl-2  col-lg-1 col-12 col-12 px-0 pt-2">
+                  <div class="text-lg-right text-center">
+                    <label class="text-secondary order-text-set-b">ชิ้น</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="px-3"><hr style="border: 1px solid #d6d6d6;"></div> 
+
+        <!-- END CONTENT -->
+      </div><!-- home-size-web -->
+
+
+
+      <div class="container">
+        <div class="cart-size-web">
+
+          <div class="row cart-coupon">
+            <div class="col-sm-3 col-12">
+              <div class="order-text-set-b text-md-left text-center mt-2">
+                  <label class="text-secondary">รหัสส่วนลด</label>
+              </div>              
+            </div>
+            <div class="col-sm-6 col-12 mb-3">
+              <div>
+                <input type="text" class="profile-form">
+              </div>
+            </div>
+            <div class="col-sm-3 col-12 mb-3">
+              <div>
+                <button type="button" class="btn orderconfirm-edit width100">ยืนยัน</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-12">
+              <div class="text-secondary text-center">
+                  <h2 class="order-setfont-h2">ยอดชำระเงิน</h2>
+              </div>
+            </div>
+            <div class="col-6">
+               <div class="order-text-set-b text-left">
+                  <label class="text-secondary">ยอดรวมค่าสินค้า</label>
+                </div>
+            </div>
+            <div class="col-6 text-md-right">
+              <div class="order-text-set-b text-right">
+                <label class="text-dark-purple">1,620 บาท</label>
+              </div>
+            </div>
+            <div class="col-6">
+               <div class="order-text-set-b text-left">
+                  <label class="text-secondary">ค่าจัดส่ง</label>
+                </div>
+            </div>
+            <div class="col-6 text-md-right">
+              <div class="order-text-set-b text-right">
+                <label class="text-dark-purple">0 บาท</label>
+              </div>
+            </div>
+
+            <div class="col-12">
+              <hr style="border: 1px solid #d6d6d6;">
+            </div>
+
+            <div class="col-6">
+               <div class="order-text-set-b text-left">
+                  <label class="text-secondary order-total-price">รวมทั้งสิ้น</label>
+                </div>
+            </div>
+            <div class="col-6 text-md-right">
+              <div class="order-text-set-b text-right">
+                <label class="text-dark-purple order-total-price">1,620 บาท</label>
+              </div>
+            </div>
+          </div>
+
+          <div class="row order-btn-my-auto">
+            <div class="col-sm-6 col-12 mb-3">
+              <div class="text-center">
+                <button type="button" class="btn bnt-white">ยกเลิก</button>
+              </div>
+            </div>
+            <div class="col-sm-6 col-12 mb-3">
+              <div class="text-center">
+                <button type="button" class="btn bnt-purple signup-btn-border-none">ยืนยันคำสั่งซื้อ</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div> <!-- wrapper -->
+
+    <!-- START FOOTER -->
+    <?php include_once('inc/footer.php'); ?>
+    <!-- END FOOTER -->
+
+    <!-- START FOOTER SCRIPT -->
+    <?php include_once('inc/footer-script.php'); ?>
+    <!-- END FOOOTER SCRIPT -->
+
+
+  </body>
+</html>
